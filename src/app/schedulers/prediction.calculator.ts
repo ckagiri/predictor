9741b72b360a4de5bf0c3bd34b6e21773d25e69f
Charles Kagiri <charleskagiri@gmail.com>
2019-06-05 15:47:41 +0300

    added prediction processor

---
+++ src/app/schedulers/prediction.calculator.ts
@@ -0,0 +1,72 @@
+import { ScorePoints, Score } from '../../common/score';
+
+export class PredictionCalculator {
+  static getInstance() {
+    return new PredictionCalculator();
+  }
+
+  calculateScore(result: Score, choice: Score): ScorePoints {
+    const scorePoints: ScorePoints = {
+      points: 0,
+      APoints: 0,
+      BPoints: 0,
+      MatchOutcomePoints: 0,
+      TeamScorePlusPoints: 0,
+      GoalDifferencePoints: 0,
+      ExactScorePoints: 0,
+      TeamScoreMinusPoints: 0
+    };
+    const choiceOutcome = calcOutcome(choice.goalsHomeTeam, choice.goalsAwayTeam);
+    const resultOutcome = calcOutcome(result.goalsHomeTeam, result.goalsAwayTeam);
+    if (choiceOutcome === resultOutcome) {
+      scorePoints.MatchOutcomePoints = 4;
+      scorePoints.APoints += 4;
+    }
+    if (choice.goalsHomeTeam === result.goalsHomeTeam) {
+      scorePoints.TeamScorePlusPoints += result.goalsHomeTeam || 1;
+      scorePoints.APoints += result.goalsHomeTeam || 1;
+    }
+    if (choice.goalsAwayTeam === result.goalsAwayTeam) {
+      scorePoints.TeamScorePlusPoints += result.goalsAwayTeam || 1;
+      scorePoints.APoints += result.goalsAwayTeam || 1;
+    }
+
+    const choiceGd = choice.goalsHomeTeam - choice.goalsAwayTeam;
+    const resultGd = result.goalsHomeTeam - result.goalsAwayTeam;
+    if (choiceGd === resultGd) {
+      scorePoints.GoalDifferencePoints = 1;
+      scorePoints.BPoints += 1;
+    }
+    if (
+      choice.goalsHomeTeam === result.goalsHomeTeam &&
+      choice.goalsAwayTeam === result.goalsAwayTeam
+    ) {
+      scorePoints.ExactScorePoints = 1;
+      scorePoints.BPoints += 1;
+    }
+    const homeGoalsGd = Math.abs(choice.goalsHomeTeam - result.goalsHomeTeam);
+    if (homeGoalsGd > 1) {
+      scorePoints.TeamScoreMinusPoints -= 1;
+      scorePoints.BPoints -= 1;
+    }
+    const awayGoalsGd = Math.abs(choice.goalsAwayTeam - result.goalsAwayTeam);
+    if (awayGoalsGd > 1) {
+      scorePoints.TeamScoreMinusPoints -= 1;
+      scorePoints.BPoints -= 1;
+    }
+    scorePoints.points = scorePoints.APoints + scorePoints.BPoints;
+    return scorePoints;
+  }
+}
+
+export default PredictionCalculator;
+
+function calcOutcome(home: number, away: number): string {
+  if (home > away) {
+    return 'w';
+  } else if (home < away) {
+    return 'l';
+  } else {
+    return 'd';
+  }
+}
---
+++ src/app/schedulers/prediction.processor.ts
@@ -0,0 +1,99 @@
+import { Observable, from, of } from 'rxjs';
+import { map, flatMap, toArray } from 'rxjs/operators';
+
+import { IFixtureRepository, FixtureRepository } from '../../db/repositories/fixture.repo';
+import { IUserRepository, UserRepository } from '../../db/repositories/user.repo';
+import { IPredictionRepository, PredictionRepository } from '../../db/repositories/prediction.repo';
+import { PredictionCalculator } from './prediction.calculator';
+
+import { IFixture } from '../../db/models/fixture.model';
+import { IPrediction, PredictionStatus } from '../../db/models/prediction.model';
+import { FootballApiProvider as ApiProvider } from '../../common/footballApiProvider';
+
+export interface IPredictionProcessor {
+  getPredictions$(fixture: IFixture): Observable<IPrediction[]>;
+  processPrediction$(prediction: IPrediction, fixture: IFixture): Observable<IPrediction>;
+}
+
+export class PredictionProcessor implements IPredictionProcessor {
+  static getInstance() {
+    return new PredictionProcessor(
+      FixtureRepository.getInstance(ApiProvider.LIGI),
+      UserRepository.getInstance(),
+      PredictionRepository.getInstance(),
+      PredictionCalculator.getInstance()
+    );
+  }
+  constructor(
+    private fixtureRepo: IFixtureRepository,
+    private userRepo: IUserRepository,
+    private predictionRepo: IPredictionRepository,
+    private predictionCalculator: PredictionCalculator
+  ) {}
+
+  getPredictions$(fixture: IFixture) {
+    const { season: seasonId, gameRound } = fixture;
+    return this.fixtureRepo
+      .findSelectableFixtures$(seasonId!, gameRound!)
+      .pipe(
+        map(selectableFixtures => {
+          return [...selectableFixtures, fixture].map(n => n.id!);
+        })
+      )
+      .pipe(
+        flatMap(fixtureIds => {
+          return this.userRepo
+            .findAll$()
+            .pipe(
+              flatMap(users => {
+                return from(users);
+              })
+            )
+            .pipe(
+              map(user => {
+                return {
+                  selectableFixtureIds: fixtureIds,
+                  user
+                };
+              })
+            );
+        })
+      )
+      .pipe(
+        flatMap(data => {
+          const selectableFixtureIds = data.selectableFixtureIds;
+          const userId = data.user.id;
+          return this.predictionRepo
+            .findOrCreateJoker$(userId!, seasonId!, gameRound!, selectableFixtureIds)
+            .pipe(
+              map(jokerPrediction => {
+                return {
+                  userId,
+                  jokerPrediction
+                };
+              })
+            );
+        })
+      )
+      .pipe(
+        flatMap(data => {
+          const fixtureId = fixture.id;
+          const { userId, jokerPrediction } = data;
+
+          if (jokerPrediction.fixture === fixtureId) {
+            return of(jokerPrediction);
+          }
+          return this.predictionRepo.findOneOrCreate$({ userId: userId!, fixtureId: fixtureId! });
+        })
+      )
+      .pipe(toArray());
+  }
+
+  processPrediction$(prediction: IPrediction, fixture: IFixture) {
+    const { choice } = prediction;
+    const { result } = fixture;
+    const scorePoints = this.predictionCalculator.calculateScore(choice, result!);
+    const status = PredictionStatus.PROCESSED;
+    return this.predictionRepo.findByIdAndUpdate$(prediction.id!, { scorePoints, status });
+  }
+}
