1a2dc9052e4366475f1132389f4954f0440ca04f
ckagiri <charleskagiri@gmail.com>
2020-03-02 02:48:28 +0300

    show 1 competition

--- backend/app/api/competitions/competitions.router.ts
+++ backend/app/api/competitions/competitions.router.ts
@@ -4,5 +4,6 @@ import competitionsController from './competitions.controller';
 const router = Router();
 
 router.get('/', competitionsController.getCompetitions);
+router.get('/:id', competitionsController.getCompetition);
 
 export const competitionsRouter = router;
