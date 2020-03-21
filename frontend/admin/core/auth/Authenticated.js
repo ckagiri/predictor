8af54069fe8d984523f0dabeac6152aa5714bc83
ckagiri <charleskagiri@gmail.com>
2020-03-21 22:36:33 +0300

    auth to js

---
+++ frontend/admin/core/auth/Authenticated.js
@@ -0,0 +1,43 @@
+import { cloneElement } from 'react';
+
+import useAuthenticated from './useAuthenticated';
+
+/**
+ * Restrict access to children to authenticated users.
+ * Redirects anonymous users to the login page.
+ *
+ * Use it to decorate your custom page components to require
+ * authentication.
+ *
+ * You can set additional `authParams` at will if your authProvider
+ * requires it.
+ *
+ * @see useAuthenticated
+ *
+ * @example
+ *     const CustomRoutes = [
+ *         <Route path="/foo" render={() =>
+ *             <Authenticated authParams={{ foo: 'bar' }}>
+ *                 <Foo />
+ *             </Authenticated>
+ *         } />
+ *     ];
+ *     const App = () => (
+ *         <Admin customRoutes={customRoutes}>
+ *             ...
+ *         </Admin>
+ *     );
+ */
+const Authenticated = ({
+  authParams,
+  children,
+  location, // kept for backwards compatibility, unused
+  ...rest
+}) => {
+  useAuthenticated(authParams);
+  // render the child even though the useAuthenticated() call isn't finished (optimistic rendering)
+  // the above hook will log out if the authProvider doesn't validate that the user is authenticated
+  return cloneElement(children, rest);
+};
+
+export default Authenticated;
---
+++ frontend/admin/core/auth/useAuthenticated.js
@@ -0,0 +1,35 @@
+import { useEffect } from 'react';
+import useCheckAuth from './useCheckAuth';
+
+const emptyParams = {};
+
+/**
+ * Restrict access to authenticated users.
+ * Redirect anonymous users to the login page.
+ *
+ * Use it in your custom page components to require
+ * authentication.
+ *
+ * You can set additional `authParams` at will if your authProvider
+ * requires it.
+ *
+ * @example
+ *     const FooPage = () => {
+ *         useAuthenticated();
+ *         return <Foo />;
+ *     }
+ *     const CustomRoutes = [
+ *         <Route path="/foo" render={() => <FooPage />} />
+ *     ];
+ *     const App = () => (
+ *         <Admin customRoutes={customRoutes}>
+ *             ...
+ *         </Admin>
+ *     );
+ */
+export default (params = emptyParams) => {
+  const checkAuth = useCheckAuth();
+  useEffect(() => {
+    checkAuth(params).catch(() => { });
+  }, [checkAuth, params]);
+};
