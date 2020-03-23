815eff3b76aa4cd484a1eeb2f5bc67e7abc29bf4
ckagiri <charleskagiri@gmail.com>
2020-03-24 02:05:22 +0300

    core to js

---
+++ frontend/admin/core/index.js
@@ -0,0 +1,13 @@
+export * from './actions';
+export * from './auth';
+export * from './core';
+export * from './loading';
+export * from './sideEffect';
+export * from './i18n';
+export * from './dataProvider';
+export * from './util';
+export * from './types';
+export * from './controller';
+export * from './export';
+
+export { getResources, getNotification } from './reducer';
