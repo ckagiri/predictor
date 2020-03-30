6afe8910f35a7ddc0e2185e245655e64ee767db8
ckagiri <charleskagiri@gmail.com>
2020-03-30 23:49:04 +0300

    add test pkgs

--- frontend/.storybook/config.js
+++ frontend/.storybook/config.js
@@ -1,6 +1,6 @@
 import { configure } from '@storybook/react';
 
-// automatically import all files ending in *.stories.js
-const req = require.context('../components', true, /\.stories\.js$/);
-
-configure(req, module);
+configure([
+  require.context('../components', true, /\.stories\.js$/),
+  require.context('../admin', true, /\.stories\.js$/)
+], module);
