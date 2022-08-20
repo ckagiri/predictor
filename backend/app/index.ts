bb5e6c949eeb64a2ffc8c69e5b3e144051dc9a33
ckagiri <ckagiri@foursquare.com>
2022-08-20 21:26:24 +0000

    worker process spike

--- backend/app/index.ts
+++ backend/app/index.ts
@@ -1,3 +1,6 @@
 import startServer from './server';
 
-startServer();
+// @ts-ignore // check if code is not running under ts-node
+// if (!process[Symbol.for("ts-node.register.instance")])
+console.log('ts-node', process[Symbol.for("ts-node.register.instance")])
+//startServer();
--- backend/app/server.ts
+++ backend/app/server.ts
@@ -3,6 +3,8 @@ import bodyParser from 'body-parser';
 import mongoose, { ConnectOptions } from 'mongoose';
 import passport from 'passport'
 import { Server } from 'http';
+import path from 'path';
+import cp from 'child_process';
 
 import { getLocalStrategy } from './api/auth/passport';
 import errorMiddleware from './api/auth/error.middleware';
@@ -22,6 +24,22 @@ async function startServer({ port = process.env.PORT } = {}): Promise<Server> {
   const mongoUri = process.env.MONGO_URI;
   const app = express();
   app.use(bodyParser.json());
+  // Fire up the child process that will run in a separate machine core
+  // and do some background processing. This way, this master process can
+  // be freed up to keep processing to a minimum on its servicing threads.
+  const pathBase = path.resolve(__dirname, '../..');
+  const base = (...paths: string[]) => path.resolve(pathBase, ...paths);
+  const fromBase = (...paths: string[]) => (...subPaths: string[]) => base(...paths, ...subPaths);
+  const fromBuildDir = fromBase('build')
+  const fromBackendDir = fromBase('backend')
+  console.log('fromBuildDir', fromBuildDir('app', 'app_FORK.js'))
+  //const node2 = cp.fork(fromBuildDir('app', 'app_FORK.js'), []);
+  const node2 = cp.fork(fromBackendDir('app', 'app_FORK.ts'), [], { execArgv: ['-r', 'ts-node/register'] })
+  node2.send({ msg: 'REFRESH_STORIES', name: 'dj wagz' });
+  app.use(function (req: any, _res, next) {
+    req.node2 = node2;
+    next();
+  });
   app.use(passport.initialize())
   passport.use(getLocalStrategy())
   app.use('/api', router);
