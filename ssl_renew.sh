578dff824f36f9fdb0c1e8be3d0bbbbcd1cf986f
ckagiri <ckagiri@foursquare.com>
2022-09-28 22:47:00 +0300

    ssl renew

---
+++ ssl_renew.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+COMPOSE="/usr/local/bin/docker-compose --ansi never"
+DOCKER="/usr/bin/docker"
+
+cd /home/ckagiri/predictor/
+$COMPOSE run certbot renew --dry-run && $COMPOSE kill -s SIGHUP ligiwebserver
+$DOCKER system prune -af
