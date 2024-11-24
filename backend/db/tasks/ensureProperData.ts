19cb287783d1427af4dcba73f394c92aa8df2e6d
ckagiri <ckagiri@foursquare.com>
2024-11-24 23:19:22 +0300

    update

--- backend/db/tasks/ensureProperData.ts
+++ backend/db/tasks/ensureProperData.ts
@@ -1,12 +1,12 @@
-import { from, lastValueFrom, map, mergeMap, of, range } from 'rxjs';
+import { from, lastValueFrom, map, mergeMap, range } from 'rxjs';
 import { CompetitionRepositoryImpl } from '../repositories/competition.repo';
 import { UserRepositoryImpl } from '../repositories/user.repo';
 import { GameRoundRepositoryImpl } from '../repositories/gameRound.repo';
 import { SeasonRepositoryImpl } from '../repositories/season.repo';
 import { MatchRepositoryImpl } from '../repositories/match.repo';
-import { concatMap, filter, last, tap, toArray } from 'rxjs/operators';
+import { concatMap, filter, last, tap } from 'rxjs/operators';
 import { PredictionRepositoryImpl } from '../repositories/prediction.repo';
-import { User } from '../models';
+import { Match, User } from '../models';
 import mongoose, { ConnectOptions } from 'mongoose';
 
 function ensureProperPredictions(users: User[]) {
@@ -31,14 +31,12 @@ function ensureProperPredictions(users: User[]) {
           filter(({ currentRound }) => !!currentRound)
         )
     }),
-    tap(({ seasonId, currentRound }) => console.log(`Creating predictions for season ${seasonId} upto round ${currentRound.position} ${currentRound.id}`)),
-     mergeMap(({ seasonId, currentRound }) => {
+    mergeMap(({ seasonId, currentRound }) => {
       return range(1, currentRound.position)
         .pipe(
           map(position => ({ seasonId, roundPosition: position }))
         )
     }),
-    tap(({ seasonId, roundPosition }) => console.log(`Creating predictions for round ${roundPosition} of season ${seasonId}`)),
     mergeMap(({ seasonId, roundPosition }) => {
       return roundRepo.findOne$({ season: seasonId, position: roundPosition })
         .pipe(
@@ -49,22 +47,20 @@ function ensureProperPredictions(users: User[]) {
       return matchRepo.findAll$({ season: seasonId, gameRound: roundId })
         .pipe(
           map(matches => {
-            return matches.map(match => ({ ...match, status: 'SCHEDULED' }))
+            return matches.map(match => ({ ...match, status: 'SCHEDULED' }) as Match);
           }),
-          map(matches => ({ seasonId, roundId, roundPosition, roundMatches: matches })),
+          map(matches => ({ roundId, roundPosition, roundMatches: matches })),
         )
     }),
-    tap(({ seasonId, roundId, roundPosition, roundMatches }) =>
-      console.log(`Creating predictions for round(${roundPosition}) ${roundId} of season ${seasonId} for ${roundMatches.length} matches`)),
-    mergeMap(({ roundId, roundMatches }) => {
+    mergeMap(({ roundId, roundPosition, roundMatches }) => {
       return from(users)
         .pipe(
           // filter roundMatches where kickoff is greater than user create date
-          map(user => ({ userId: user.id!, roundId, roundMatches })),
+          map(user => ({ userId: user.id!, roundId, roundPosition, roundMatches })),
         )
     }),
-    tap(({ userId, roundId, roundMatches }) =>
-      console.log(`Creating predictions for user ${userId} and round ${roundId} for ${roundMatches.length} matches`)),
+    tap(({ userId, roundId, roundPosition, roundMatches }) =>
+      console.log(`Creating predictions for user ${userId} and round(${roundPosition}) ${roundId} for ${roundMatches.length} matches`)),
     concatMap(({ userId, roundMatches }) => {
       return predictionRepo.findOrCreatePredictions$(userId, roundMatches);
     }),
