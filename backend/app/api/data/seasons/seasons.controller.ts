b9a65ae1128b0eff2ed8c8124414520fb40dee75
Charles Kagiri <charleskagiri@gmail.com>
2025-05-19 12:31:52 +0300

    update season response

--- backend/app/api/data/seasons/seasons.controller.ts
+++ backend/app/api/data/seasons/seasons.controller.ts
@@ -1,10 +1,13 @@
-import { Request, Response } from 'express';
-import { lastValueFrom } from 'rxjs';
+import { Request, Response } from "express";
+import { lastValueFrom } from "rxjs";
 import {
   SeasonRepositoryImpl,
   SeasonRepository,
-} from '../../../../db/repositories/season.repo';
-import { TeamRepository, TeamRepositoryImpl } from '../../../../db/repositories/team.repo';
+} from "../../../../db/repositories/season.repo";
+import {
+  TeamRepository,
+  TeamRepositoryImpl,
+} from "../../../../db/repositories/team.repo";
 
 export class SeasonsController {
   static getInstance(
@@ -17,18 +20,20 @@ export class SeasonsController {
   constructor(
     private seasonRepo: SeasonRepository,
     private teamRepo: TeamRepository
-  ) { }
+  ) {}
 
   public getSeasons = async (req: Request, res: Response) => {
     try {
-      const competition = req.params.competition
+      const competition = req.params.competition;
       if (!competition) {
-        throw new Error('competition slug is required');
+        throw new Error("competition slug is required");
       }
-      const seasons = await lastValueFrom(this.seasonRepo.findAll$(
-        { 'competition.slug': competition },
-        '-createdAt -externalReference -teams'
-      ));
+      const seasons = await lastValueFrom(
+        this.seasonRepo.findAll$(
+          { "competition.slug": competition },
+          "-createdAt -externalReference -teams"
+        )
+      );
       return res.status(200).json(seasons);
     } catch (error) {
       return res.status(500).send(error);
@@ -40,30 +45,36 @@ export class SeasonsController {
       const competitionSlug = req.params.competition;
       const seasonSlug = req.params.slug;
       if (!competitionSlug) {
-        throw new Error('competition slug is required');
+        throw new Error("competition slug is required");
       }
       if (!seasonSlug) {
-        throw new Error('season slug is required');
+        throw new Error("season slug is required");
       }
 
-      const season = await lastValueFrom(this.seasonRepo.findOne$({
-        'competition.slug': competitionSlug, slug: seasonSlug
-      }, '-createdAt -externalReference'));
+      const season = await lastValueFrom(
+        this.seasonRepo.findOne$(
+          {
+            "competition.slug": competitionSlug,
+            slug: seasonSlug,
+          },
+          "-createdAt -externalReference"
+        )
+      );
       if (!season) {
-        throw new Error('season not found');
+        throw new Error("season not found");
       }
 
       // TODO: use populate
-      const teams = await lastValueFrom(this.teamRepo.findAllByIds$(season.teams as string[]));
+      const teams = await lastValueFrom(
+        this.teamRepo.findAllByIds$(season.teams as string[])
+      );
       season.teams = teams;
 
-      res.status(200).json({
-        season,
-      })
+      res.status(200).json(season);
     } catch (error) {
       res.status(500).send(error);
     }
-  }
+  };
 }
 
 const seasonsController = SeasonsController.getInstance();
