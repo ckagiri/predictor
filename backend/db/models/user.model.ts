b355db78a5a38231e96b6879c7dc3d1a537d3d86
Charles Kagiri <charleskagiri@gmail.com>
2025-07-01 00:45:50 +0300

    additional cleanup

--- backend/db/models/user.model.ts
+++ backend/db/models/user.model.ts
@@ -23,40 +23,6 @@ const userSchema = schema({
   username: { lowercase: true, required: true, type: String, unique: true },
 }) as Schema<User>;
 
-// userSchema.pre('save', function (next) {
-//   if (!this.isModified('password')) {
-//     next();
-//     return;
-//   }
-//   bcrypt.genSalt(process.env.NODE_ENV !== 'test' ? 10 : 1, (err, salt) => {
-//     if (err) {
-//       next(err);
-//       return;
-//     }
-//     if (typeof salt !== 'string') {
-//       next(new Error('Failed to generate salt'));
-//       return;
-//     }
-//     bcrypt.hash(this.password!, salt, (err, hash) => {
-//       if (err) {
-//         next(err);
-//         return;
-//       }
-//       this.password = hash;
-//       next();
-//     });
-//   });
-// });
-
-// userSchema.methods.comparePassword = function comparePassword(
-//   candidatePassword: string,
-//   cb: (err: any, isMatch: unknown) => void
-// ) {
-//   bcrypt.compare(candidatePassword, this.password ?? '', (err, isMatch) => {
-//     cb(err, isMatch);
-//   });
-// };
-
 const UserModel = model<User>('User', userSchema);
 
 export default UserModel;
