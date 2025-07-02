75a65d4f1ac577474ced55e2470ed13f01ca6fe3
Charles Kagiri <charleskagiri@gmail.com>
2025-07-02 20:51:47 +0300

    update pick-score ctrl & useCase

---
+++ backend/app/api/contests/roundMatches/pickScore.controller.ts
@@ -0,0 +1,54 @@
+import { Response } from 'express';
+
+import AppError from '../../common/AppError.js';
+import HttpRequestModel from '../../common/interfaces/HttpRequestModel.js';
+import OkResponder from '../../common/responders/ok.responder.js';
+import Result from '../../common/result/index.js';
+import Validator from '../../common/validation/validator.js';
+import { getRoundMatchesValidator as getMatchesValidator } from '../controller.validators.js';
+import PickScoreUseCase, {
+  RequestModel,
+} from './useCases/pickScore.useCase.js';
+
+class PickScoreController {
+  constructor(
+    private readonly pickScoreUseCase: PickScoreUseCase,
+    private readonly validation: Validator
+  ) {}
+
+  static getInstance(
+    pickScoreUseCase: PickScoreUseCase,
+    validation = getMatchesValidator
+  ) {
+    return new PickScoreController(pickScoreUseCase, validation);
+  }
+
+  async processRequest(request: HttpRequestModel): Promise<void> {
+    const loggedInUserId = request.auth?.id as string | undefined;
+    if (!loggedInUserId) {
+      throw Result.fail(AppError.unauthorized());
+    }
+
+    const { competition, round, season } = request.params;
+    const predictionSlip = request.body as Record<string, string>;
+    const requestValidated = await this.validation.validate<RequestModel>({
+      competition,
+      loggedInUserId,
+      predictionSlip,
+      round,
+      season,
+    });
+    if (requestValidated.isFailure) {
+      throw Result.fail(requestValidated.unwrap() as AppError, 'Bad Request');
+    }
+
+    const requestModel = requestValidated.value!;
+    await this.pickScoreUseCase.execute(requestModel);
+  }
+}
+
+export const makePickScoreController = (res: Response) => {
+  const okResponder = new OkResponder(res);
+  const pickScoreUseCase = PickScoreUseCase.getInstance(okResponder);
+  return PickScoreController.getInstance(pickScoreUseCase);
+};
---
+++ backend/app/api/contests/roundMatches/useCases/pickScore.useCase.ts
@@ -0,0 +1,99 @@
+import { lastValueFrom } from 'rxjs';
+
+import { Score } from '../../../../../common/score.js';
+import { Match, Prediction } from '../../../../../db/models/index.js';
+import { CompetitionRepositoryImpl } from '../../../../../db/repositories/competition.repo.js';
+import { GameRoundRepositoryImpl } from '../../../../../db/repositories/gameRound.repo.js';
+import { MatchRepositoryImpl } from '../../../../../db/repositories/match.repo.js';
+import { PredictionRepositoryImpl } from '../../../../../db/repositories/prediction.repo.js';
+import { SeasonRepositoryImpl } from '../../../../../db/repositories/season.repo.js';
+import { UserRepositoryImpl } from '../../../../../db/repositories/user.repo.js';
+import AppError from '../../../common/AppError.js';
+import Responder from '../../../common/responders/Responder.js';
+import Result from '../../../common/result/index.js';
+import GetRoundMatchesUseCase from './getRoundMatches.useCase.js';
+
+export interface RequestModel {
+  competition: string;
+  loggedInUserId: string;
+  predictionSlip: Record<string, string>;
+  round?: string;
+  season?: string;
+}
+
+export default class PickScore extends GetRoundMatchesUseCase {
+  static getInstance(
+    responder: Responder,
+    competitionRepo = CompetitionRepositoryImpl.getInstance(),
+    seasonRepo = SeasonRepositoryImpl.getInstance(),
+    roundRepo = GameRoundRepositoryImpl.getInstance(),
+    matchRepo = MatchRepositoryImpl.getInstance(),
+    userRepo = UserRepositoryImpl.getInstance(),
+    predictionRepo = PredictionRepositoryImpl.getInstance()
+  ) {
+    return new PickScore(
+      responder,
+      competitionRepo,
+      seasonRepo,
+      roundRepo,
+      matchRepo,
+      userRepo,
+      predictionRepo
+    );
+  }
+  async execute({
+    competition,
+    loggedInUserId,
+    predictionSlip,
+    round,
+    season,
+  }: RequestModel): Promise<void> {
+    try {
+      const foundCompetition = await this.findCompetition(competition);
+      const foundSeason = await this.findSeason(foundCompetition, season);
+      const [foundRound] = await this.findRound(foundSeason, round);
+
+      const roundMatches = (await this.getRoundMatches(
+        foundRound.id!
+      )) as Match[];
+      const picks = await this.pickScores(
+        loggedInUserId,
+        roundMatches,
+        predictionSlip
+      );
+      this.responder.respond(picks);
+    } catch (err: any) {
+      if (err.isFailure) {
+        throw err;
+      }
+
+      throw Result.fail(
+        AppError.create('request-failed', 'Failed to make prediction', err),
+        'Internal Server Error'
+      );
+    }
+  }
+
+  private async pickScores(
+    userId: string,
+    roundMatches: Match[],
+    predictionSlip: Record<string, string>
+  ) {
+    const picks = [] as Prediction[];
+    for (const [matchSlug, choice] of Object.entries(predictionSlip)) {
+      const match = roundMatches.find(m => m.slug === matchSlug);
+      if (match == undefined) continue;
+      const score = choice.split('-');
+      const goalsHomeTeam = Number(score[0]);
+      const goalsAwayTeam = Number(score[1]);
+      const scoreChoice: Score = { goalsAwayTeam, goalsHomeTeam };
+      const pick = await lastValueFrom(
+        this.predictionRepo.pickScore$(userId, match, roundMatches, scoreChoice)
+      );
+      if (pick) {
+        picks.push(pick);
+      }
+    }
+    return picks;
+  }
+}
