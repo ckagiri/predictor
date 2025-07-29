c6100ff69a6b27f6c5e83cf74204e82ac4a1ba9c
Charles Kagiri <charleskagiri@gmail.com>
2025-07-29 09:51:50 +0300

    refactor(wip): docker compose dev

--- make/dev-compose.mk
+++ make/dev-compose.mk
@@ -3,28 +3,23 @@
 #
 compose-dev-build:
 	docker compose -f ./docker-compose-dev.yml \
-		-p ligi-dev \
 		--env-file ./config/server/dev/server.env \
 		build
 
 compose-dev-up:
 	docker compose -f ./docker-compose-dev.yml \
-		-p ligi-dev \
-		--env-file ./config/server/dev/server.env \
-		up -d
-
-compose-dev-up1:
-	docker compose -f ./docker-compose-dev.yml \
 		--env-file ./config/server/dev/server.env \
 		up -d
 
 compose-dev-down:
-	docker compose -f ./docker-compose-dev.yml -p ligi-dev down
-
-compose-dev-build-up:
 	docker compose -f ./docker-compose-dev.yml \
-		--env-file ./config/server/dev/server.env
-			---build -d
+		--env-file ./config/server/dev/server.env \
+		down
+
+compose-dev-down_v:
+	docker compose -f ./docker-compose-dev.yml \
+		--env-file ./config/server/dev/server.env \
+		down -v
 
 compose-dev-clean: compose-dev-down
 	docker rm -f ligi-dev
