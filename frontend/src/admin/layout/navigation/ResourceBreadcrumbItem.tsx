e87a0cd5f99fb1e6f2960d4d2dfade81add622c5
Charles Kagiri <charleskagiri@gmail.com>
2025-07-30 11:45:26 +0300

    fix(web): team link works

--- frontend/src/admin/layout/navigation/ResourceBreadcrumbItem.tsx
+++ frontend/src/admin/layout/navigation/ResourceBreadcrumbItem.tsx
@@ -1,7 +1,7 @@
-import { ReactElement } from "react";
-import { BreadcrumbItem, BreadcrumbPath } from "../../../ui-materialui";
-import { useResourceBreadcrumbPaths } from "./useResourceBreadcrumbPaths";
-import { ResourceItem } from "../../../frame";
+import { ReactElement } from 'react';
+import { BreadcrumbItem, BreadcrumbPath } from '../../../ui-materialui';
+import { useResourceBreadcrumbPaths } from './useResourceBreadcrumbPaths';
+import { ResourceItem } from '../../../frame';
 
 export type ResourceBreadcrumbItemProps = {
   resource: ResourceItem;
@@ -11,24 +11,23 @@ export type ResourceBreadcrumbItemProps = {
 export const ResourceBreadcrumbItem = ({
   resource,
 }: ResourceBreadcrumbItemProps): ReactElement => {
-  const resourcesPaths = useResourceBreadcrumbPaths(resource);
-  const resourceRoute = resource.route ?? "";
-  const resourcePathKey = resourceRoute.replace(/\/:[^:/]+\//g, ".edit.");
-  const listPath = resourcesPaths[resourcePathKey];
-  const childPaths = Object.keys(resourcesPaths)
-    .filter((pathKey) => pathKey !== resourcePathKey)
+  const resourcePaths = useResourceBreadcrumbPaths(resource);
+  const resourceRoute = resource.route ?? '';
+  const resourcePathKey = resourceRoute.replace(/\/:[^:/]+\//g, '.edit.');
+  const listPath = resourcePaths[resourcePathKey];
+  const childPaths = Object.keys(resourcePaths)
+    .filter(pathKey => pathKey !== resourcePathKey)
     .reduce(
       (acc, pathKey) => ({
         ...acc,
-        [pathKey.substring(resourcePathKey.length + 1)]:
-          resourcesPaths[pathKey],
+        [pathKey.substring(resourcePathKey.length + 1)]: resourcePaths[pathKey],
       }),
       {}
     ) as Record<string, BreadcrumbPath>;
 
   return (
     <BreadcrumbItem name={resourcePathKey} {...listPath}>
-      {Object.keys(childPaths).map((pathKey) => (
+      {Object.keys(childPaths).map(pathKey => (
         <BreadcrumbItem key={pathKey} name={pathKey} {...childPaths[pathKey]} />
       ))}
     </BreadcrumbItem>
