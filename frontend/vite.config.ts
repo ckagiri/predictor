43b3e5371cd2235a0fa2ab1f55fb30bb382b7af0
Charles Kagiri <charleskagiri@gmail.com>
2025-07-30 11:10:26 +0300

    refactor(wip): added back nginx

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ['web'],
     host: true, // crucial for container accessibility
     port: 8040,
   },
