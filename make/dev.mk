19b7935f879794e821e47aea4d188e11fb7552f8
Charles Kagiri <ckagiri@foursquare.com>
2025-08-02 11:13:36 +0300

    build: update prod compose file (#51)

--- make/dev.mk
+++ make/dev.mk
@@ -1,11 +1,6 @@
 #
 # dev
 #
-
-all-dev-build: client-dev-build server-dev-build nginx-dev-build
-
-all-dev-clean: all-dev-images-clean all-dev-containers-clean
-
 all-dev-containers-clean:
 	docker rm -f ligi-client-dev
 	docker rm -f ligi-server-dev
@@ -19,12 +14,6 @@ all-dev-images-clean:
 #
 # client
 #
-
-client-dev-build:
-	docker build -f ./config/client/dev/Dockerfile \
-		-t ligi-client-dev-image:latest \
-		.
-
 client-dev-sh:
 	docker run --rm --name ligi-client-dev \
 		-it \
@@ -42,18 +31,7 @@ client-dev-run:
 #
 # server
 #
-
-server-dev-build:
-	docker build -f ./config/server/dev/Dockerfile \
-	-t ligi-server-dev-image:latest \
-	.
-
 server-dev-sh:
-	docker run --rm --name ligi-server-dev \
-		-it ligi-server-dev-image \
-		sh
-
-server-dev-sh1:
 	docker run --rm --name ligi-server-dev \
 		-it ligi-server-dev-image \
 		sh
@@ -65,26 +43,3 @@ server-dev-run:
 		-it \
 		-p 3110:3110 \
 		ligi-server-dev-image:latest
-
-#
-# nginx
-#
-
-nginx-dev-build:
-	docker build -f ./config/nginx/Dockerfile \
-		-t ligi-nginx-dev-image:latest \
-		.
-
-nginx-dev-sh:
-	docker run --rm --name ligi-nginx-dev \
-		-it \
-		ligi-nginx-dev-image \
-		sh
-
-nginx-dev-run:
-	docker run \
-		--rm \
-		--name ligi-nginx-dev \
-		-it \
-		-p 8100:8100 \
-		ligi-nginx-dev-image:latest
--- ssl_renew.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-COMPOSE="/usr/bin/docker compose"
-DOCKER="/usr/bin/docker"
-
-!/bin/bash
-cd /home/ckagiri/predictor/
-$COMPOSE run certbot certonly --webroot --webroot-path=/var/www/html --email charleskagiri@gmail.com --agree-tos --no-eff-email --force-renewal -d ligipredictor.com  -d www.ligipredictor.com
-$COMPOSE restart
